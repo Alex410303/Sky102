f7bf5d70dad838d99c806b479f711a528fcc6b6a
Alex410303 <Alex410303@gmail.com>
2014-10-30 23:17:00 +0200

    Recr

---
+++ Prog2Tank/Assets/GameSkyWar/Scr/Rocked.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rocked : MonoBehaviour {
+
+
+	public int life = 100;
+
+	void Start () 
+	{
+		//rigidbody2D.AddRelativeForce (new Vector2(0,1)*100);
+		//AddRelativeForce (new Vector2 (0, 1) * SpeedMove);
+
+		//Destroy (gameObject,5);
+		//transform.Rotate (0,0,transform.parent.transform.rotation.z);
+			//rigidbody2D.transform.Rotate (0, 0, transform.position.z+right);
+		//GameObject kkk = transform.parent.transform.rotation.z;
+		//Debug.Log ("m "+transform.rotation.z);
+		//Debug.Log ("l "+transform.localPosition);
+	}
+	void Update ()
+	{
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.name != "Char1")
+		{
+			Destroy (other.gameObject);
+			Destroy(gameObject);
+		}
+
+	}
+}
